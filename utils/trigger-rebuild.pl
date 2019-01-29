f1775e3d0a915b52d55f36ded25d810c2c216961
floraline <floraline@pobox.com>
2019-01-29 01:44:28 -0500

    Add beta version 0.23

--- utils/trigger-rebuild.pl
+++ utils/trigger-rebuild.pl
@@ -151,10 +151,10 @@ sub main() {
   my $ver = param('v');
 
   if (not $ver) {
-    do_prompt 'trunk', '0.22', 'hellcrawl', 'bcrawl';
+    do_prompt 'trunk', '0.23', '0.22', 'hellcrawl', 'bcrawl';
   } elsif ($ver eq 'trunk') {
     do_update 'trunk';
-  } elsif ($ver =~ /^0.22$|^hellcrawl$|^bcrawl$/) {
+  } elsif ($ver =~ /^0.23$|^0.22$|^hellcrawl$|^bcrawl$/) {
     do_update 'stable', $ver;
   } else {
     do_fail "Unknown version " . escapeHTML($ver);
