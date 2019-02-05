422c75235d1ec5c6ccca04876b33577db0e92584
floraline <floraline@pobox.com>
2019-02-05 17:43:44 -0500

    Run git clean prior to checkout
    This ensures a nice clean source directory ready for the new
    branch to build. I was having rare issues where sometimes
    git checkout halted the update-stable script because it refused
    to check out files on top of untracked local changes. This should
    help avoid such cases.
    
    I think some kind of git reset might also be appropriate here
    to better support cases where someone force-pushes to one of
    my remotes and rewrites history. update-stable will try to
    fast-forward which is not guaranteed to work.

--- crawl-build/update-public-repository.sh
+++ crawl-build/update-public-repository.sh
@@ -19,6 +19,7 @@ clone-crawl-ref() {
 update-crawl-ref() {
     say "Updating git repository $REPO_DIR"
     ( cd $REPO_DIR && git checkout -f &&
+        git clean -xdf &&
         git checkout $BRANCH &&
         git pull )
     if [[ -n "$REVISION" ]]; then
